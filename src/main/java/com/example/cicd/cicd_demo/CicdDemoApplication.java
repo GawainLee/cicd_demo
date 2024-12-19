280ae22af05b597a769ac2e96d13f152bc3a455e
gawain <gawain@aishk.com>
2024-12-19 17:39:17 +0800

    init folder

---
+++ src/main/java/com/example/cicd/cicd_demo/CicdDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.cicd.cicd_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDemoApplication.class, args);
+		System.out.println("CI/CD demo start...");
+	}
+
+}
